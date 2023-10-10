6d8d7b52032979249a160aa71156834d76786a4a
ridipblog <memorytemp5@gmail.com>
2023-10-10 17:34:02 +0530

    Add 10-10-23 2

--- DCAUA/resources/views/components/state-dash-componenet.blade.php
+++ DCAUA/resources/views/components/state-dash-componenet.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex justify-content-around align-items-center flex-wrap p-0 m-0">
     <div class="d-flex col-md-4 col-sm-12 flex-column  m-2 border rounded nav_box pt-3 pb-3">
-        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-plus"></i></h6>
+        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-user"></i></h6>
         <a href="/add_ceo" class=" text-decoration-none col-12 text-center text-white">
             ADD CEO/PD LOGIN</a>
     </div>
@@ -10,7 +10,7 @@
         LOGIN</a> --}}
 
     <div class="d-flex col-md-4 col-sm-12 flex-column m-2 border rounded nav_box pt-3 pb-3">
-        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-plus"></i></h6>
+        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-user"></i></h6>
         <a href="/add_po" class=" text-decoration-none col-12 text-center text-white">
             ADD PROGRAM OFFICER LOGIN</a>
     </div>
@@ -19,7 +19,7 @@
             class="fa-solid fa-plus"></i> ADD PROGRAM OFFICER LOGIN</a> --}}
 
     <div class="d-flex col-md-4 col-sm-12 flex-column m-2 border rounded nav_box pt-3 pb-3">
-        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-plus"></i></h6>
+        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-list"></i></h6>
         <a href="/list_ceo" class=" text-decoration-none col-12 text-center text-white">
             LIST CEO/PD LOGIN</a>
     </div>
@@ -27,7 +27,7 @@
         class="nav_box text-decoration-none col-md-4 col-sm-12 border text-center pt-4 m-3 pb-4 text-white"><i
             class="fa-solid fa-list"></i> LIST CEO/PD LOGIN</a> --}}
     <div class="d-flex col-md-4 col-sm-12 flex-column m-2 border rounded nav_box pt-3 pb-3">
-        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-plus"></i></h6>
+        <h6 class="fs-2 col-12 text-center text-white"><i class="fa-solid fa-list"></i></h6>
         <a href="/list_po" class=" text-decoration-none col-12 text-center text-white">
             LIST PROGRAM OFFICER LOGIN</a>
     </div>
--- DCAUA/resources/views/state/list_ceo.blade.php
+++ DCAUA/resources/views/state/list_ceo.blade.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             {{-- Header For State Dashboard --}}
 
